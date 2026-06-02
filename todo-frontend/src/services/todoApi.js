781e1126e0420c2c385a5189f11ef322417badb9
DeepRulz <sdeep272006@gmail.com>
2026-06-02 18:56:00 +0530

    Updated Local Host ID

--- todo-frontend/src/services/todoApi.js
+++ todo-frontend/src/services/todoApi.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL
-});
+    baseURL: import.meta.env.VITE_URL
+})
 
 export const getTodos = () => API.get("/");
 
